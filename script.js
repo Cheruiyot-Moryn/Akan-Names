684528892b83ca381ceb2438544501b180687263
Moureen Chepkoech <moureen.chepkoech@student.moringaschool.com>
2022-03-03 21:22:59 +0300

    function

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 var CC, YY, DD, d, dayValue;
-var dayNames =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-var femaleNames =["Akosua", "Adwoa","Abenaa", "Akua","Yaa", "Afua", "Ama"]
-var maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "KOfi", "Kwame"]
+var dayNames =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var femaleNames =["Akosua", "Adwoa","Abenaa", "Akua","Yaa", "Afua", "Ama"];
+var maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "KOfi", "Kwame"];
+
+function getGender(){
+    var genders = document.getElementsByName("gender");
+    
+    for (let gender of genders){
+        if (gender.checked){
+          return gender.value;
+        }
+      }
+}
